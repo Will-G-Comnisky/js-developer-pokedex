42e7488f402c07be27e0a11c323e076e597f5a76
William <william.gispiela@gmail.com>
2024-01-16 20:19:34 -0300

    feat: main: pokemon details popup implemented, project finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,91 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 251
+const limit = 15
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
+function loadPokemonItens(offset, limit) {  
+    pokeApi.getPokemons(offset, limit).then((pokemons =[])  => {  
+        const newHtml = pokemons.map((pokemon) =>  `
+        <li class="pokemon ${pokemon.type}" onClick="selectPokemon(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
+            
+            
+            <div class="detail">
+                <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                    
+                <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
+                    
+                </div>
+        </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+    
+}
+
+const selectPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    displayPopup(pokemon)
+}
+
+const displayPopup = (pokemon) =>{
+   
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+ 
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const htmlString = `
+   
+    <div id="popup">
+        <div id="detailPokemon">
+            <button id="closeButton" onClick="closePopup()"> x </button>
+            <li class="pokemon ${pokemon.type}">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#${pokemon.id.toString().padStart(3,"0")}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
             </div>
-        </li>
+                <img id="img-pokemon" src="${photo}"alt="${pokemon.name}">
+                
+                <div id="data">
+                <h4>BASE STATS</h4>
+                <div id="hability">
+                <div class="stat-desc">
+                    ${pokemon.stats.map((name_stats) =>`<p class="${type}">${name_stats.stat.name}</p>`).join('')}
+                </div>
+                <div class="bar-inner"> ${pokemon.stats.map((base_stats) =>`<p class="${type}">${base_stats.base_stat}</p>`).join('')}</div>
+                </div>
+                <div id="stats">
+                    <div class="stat-bar">
+                        <p>Height: ${(pokemon.height/10).toFixed(2)}m</p>
+                        <p>Weight: ${(pokemon.weight/10)}kg</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </li>
     `
+    pokemonList.innerHTML = htmlString + pokemonList.innerHTML
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+const closePopup = () =>{
+    const popup = document.getElementById('popup')
+    popup.parentElement.removeChild(popup)
 }
 
 loadPokemonItens(offset, limit)
